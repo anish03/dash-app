368e7a5f35b182860539d7a39ade509bfe978f0f
anish03 <anish.narkhede@gmail.com>
2018-05-30 12:35:17 -0700

    function callback implementation

---
+++ callback_graph.py
@@ -0,0 +1,65 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
+
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/'
+    'datasets/master/gapminderDataFiveYear.csv')
+
+app = dash.Dash()
+
+# include CSS
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+app.layout = html.Div([
+    dcc.Graph(id='main-graph'),
+    dcc.Slider(
+        id='main-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        step=None,
+        marks={str(year) : str(year) for year in df['year'].unique()}
+    )
+])
+
+@app.callback(
+    Output('main-graph','figure'),
+    [Input('main-slider','value')]
+)
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(go.Scatter(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+    print traces
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest'
+        )
+    }
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
